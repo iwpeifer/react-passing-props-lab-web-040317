a6bc1cb6317e81d814862c8d0ea44cb5b3a058c5
iwpeifer <iwpeifer@gmail.com>
2017-06-09 16:41:00 -0400

    My fruitbasket works exactly like it did before I refactored it so that's cool I guess

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+FruitBasket.defaultProps = {
+  fruit: [],
+  filters: [],
+  currentFilter: null,
+  updateFilterCallback: () => {}
+}
+
+class App extends Component  {
+  constructor() {
+    super();
+
+    this.state = {
+      fruit: [],
+      filters: [],
+      currentFilter: null,
+      items: []
+    }
+    this.handleFilterChange = this.handleFilterChange.bind(this)
+    this.fetchFilters = this.fetchFilters.bind(this)
+  }
+
+  handleFilterChange = event => {
+    console.log('new filter: ', event.target.value);
+    this.setState({ selectedFilter: event.target.value });
+  }
+
+  componentWillMount() {
+    this.fetchFilters();
+    fetch('/api/fruit')
+      .then(response => response.json())
+      .then(items => this.setState({ items }));
+  }
+
+  fetchFilters = () => {
+    fetch('/api/fruit_types')
+      .then(response => response.json())
+      .then(filters => this.setState({ filters }));
+  }
+
+  render() {
+    return (
+      <FruitBasket filters={this.state.filters} selectedFilter={this.state.selectedFilter} handleFilterChange={this.handleFilterChange} items={this.state.items}/>
+    )
+  }
+
+}
 
 export default App;
